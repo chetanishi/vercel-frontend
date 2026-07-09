764cc83d677d0786a77f7bfb7c6310e9e06e3acc
chetan ishi <chetanishi986@gmail.com>
2026-07-09 22:27:04 +0530

    ok

--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -14,13 +14,13 @@ function ProductDisplay(props)
         <div className="productdisplay">
             <div className="productdisplay-left">
                 <div className="productdisplay-img-list">
-                    <img src={product.image} alt="" />
-                    <img src={product.image} alt="" />
-                    <img src={product.image} alt="" />
-                    <img src={product.image} alt="" />
+                   <img src={`https://vercel-backend-umber-kappa.vercel.app${product.image}`} alt={product.name}/>
+                   <img src={`https://vercel-backend-umber-kappa.vercel.app${product.image}`} alt={product.name}/>
+                   <img src={`https://vercel-backend-umber-kappa.vercel.app${product.image}`} alt={product.name}/>
+                   <img src={`https://vercel-backend-umber-kappa.vercel.app${product.image}`} alt={product.name}/>
                 </div>
                 <div className="productdisplay-img">
-                    <img className="productdisplay-main-img" src={product.image} alt="" />
+            <img src={`https://vercel-backend-umber-kappa.vercel.app${product.image}`} alt={product.name}/>
                 </div>
             </div>
             <div className="productdisplay-right">
