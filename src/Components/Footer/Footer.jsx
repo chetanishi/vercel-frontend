dd888f20627c921f3a6f2cc5955c877fb3c53ec1
chetan ishi <chetanishi986@gmail.com>
2026-07-09 18:16:25 +0530

    added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,6 @@
 import "./Footer.css";
 import footer_logo from "../Assets/logo_big.png";
 import instagram_icon from "../Assets/instagram_icon.png"
-import pintester_icon from "../Assets/pintester_icon.png"
 import whatsapp_icon from "../Assets/whatsapp_icon.png"
 
 function Footer()
@@ -22,9 +21,6 @@ function Footer()
             <div className="footer-social-icon">
                 <div className="footer-icons-container">
                     <img src={instagram_icon} alt="" />
-                </div>
-                 <div className="footer-icons-container">
-                    <img src={pintester_icon} alt="" />
                 </div>
                  <div className="footer-icons-container">
                     <img src={whatsapp_icon} alt="" />
